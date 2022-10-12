932cf064ba6685d13fc2103c1edda129e1da3473
abdelhai95 <abdelhaijamal95@gamil.com>
2022-10-12 15:52:15 +0200

    adding files

---
+++ script.js
@@ -0,0 +1,19 @@
+const addPlaying = function (e) {
+  // console.log(e);
+  const audio = document.querySelector(`audio[data-key= '${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key= '${e.keyCode}']`);
+  if (!audio) return;
+  audio.currentTime = 0; // rewind audio
+  audio.play();
+  key.classList.add("playing");
+};
+
+const removePlaying = function (e) {
+  const key = document.querySelector(`.key[data-key= '${e.keyCode}']`);
+  key.classList.remove("playing");
+};
+
+// const removePlayingBinded = removePlaying.bind(key);
+const keys = document.querySelectorAll(".key");
+window.addEventListener("keyup", removePlaying);
+window.addEventListener("keydown", addPlaying);
